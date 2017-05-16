273f8614fc4238f7422dc509e88689bdeba61b9f
Nyokoh <nicaetinismo@yahoo.com>
2017-05-16 08:47:43 -0700

    trying some stuff for handlebars

--- server.js
+++ server.js
@@ -89,3 +89,15 @@ app.use('/services', services)
 app.listen(process.env.PORT || 5000, () => {
     console.log('Server started.')
 })
+
+
+
+Handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
+    if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+    if( lvalue!=rvalue ) {
+        return options.inverse(this);
+    } else {
+        return options.fn(this);
+    }
+});
