d3936f246b38c7c9ec26ae01dcc617d5d92fe98e
Nyokoh <nicaetinismo@yahoo.com>
2017-05-16 10:08:38 -0700

    reverting changes

--- routes/profiles.js
+++ routes/profiles.js
@@ -13,7 +13,7 @@ router.get('/:profile', (req, res) => {
     console.log('params: '+req.params.profile)
     User.findOne({'contact_info.email': req.params.profile}, (err, user) => {
         if (err) throw err
-        res.render('search/profile_view', {user: req.user.toJSON()})
+        res.render('search/profile_view', {user})
     })
 })
 
