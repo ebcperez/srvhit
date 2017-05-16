bead1c415cf8058e422fe617b3f3e1a74bd2a749
Nyokoh <nicaetinismo@yahoo.com>
2017-05-16 08:46:45 -0700

    reverting changes

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,6 @@ const passport = require('passport')
 //get homepage
 router.get('/', function(req, res) {
     res.render('index')
-});
-
-Handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
-    if (arguments.length < 3)
-        throw new Error("Handlebars Helper equal needs 2 parameters");
-    if( lvalue!=rvalue ) {
-        return options.inverse(this);
-    } else {
-        return options.fn(this);
-    }
-});
+})
 
 module.exports = router
